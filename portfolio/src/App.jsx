d276df908402b40cf7396a897fd9d810653737f0
Vinayaraj K K <vinayarajh1@gmail.com>
2025-06-27 17:56:22 +0530

    Initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="max-w-prose mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Lorem Ipsum Test</h1>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+      </p>
+      <p className="mb-4">
+        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod.
+      </p>
+      <p>
+        Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.
+      </p>
+    </div>
+  )
+}
+
+export default App
